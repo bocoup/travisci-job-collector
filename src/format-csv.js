7aeed20c4e3b02cf3c4b0316a23749f10e50452c
Mike Pennisi <mike@mikepennisi.com>
2017-06-22 17:31:12 -0400

    Add column labels to CSV output

--- src/format-csv.js
+++ src/format-csv.js
@@ -2,16 +2,19 @@
 var productRe = /^#(.*)#$/m;
 
 module.exports = function(results) {
-  return results.map((result) => {
+  var head = 'Pull request ID,Created at,Comment posted at,Product';
+  var body = results.map((result) => {
       return result.comments
         .filter((comment) =>  comment.user.login === 'w3c-bots')
         .map((comment) => [
             result.pullRequest.number,
-            result.pullRequest.created_at,
-            comment.created_at,
+            new Date(result.pullRequest.created_at).getTime(),
+            new Date(comment.created_at).getTime(),
             comment.body.match(productRe)[1].trim()
           ].join(','));
     })
     .reduce((flattened, comments) => flattened.concat(comments), [])
     .join('\n');
+
+  return head + '\n' + body;
 };
