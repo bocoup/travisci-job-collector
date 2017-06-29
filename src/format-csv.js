f76b47fa524131e297b9981c8fb7975ef34cd13f
Mike Pennisi <mike@mikepennisi.com>
2017-06-29 12:03:38 -0400

    Omit timings for jobs that did not run tests

--- src/format-csv.js
+++ src/format-csv.js
@@ -1,11 +1,13 @@
 'use strict';
 var productRe = /^#(.*)#$/m;
+var noTestRe = /No tests run./;
 
 module.exports = function(results) {
   var head = 'Pull request ID,Created at,Comment posted at,Product';
   var body = results.map((result) => {
       return result.comments
         .filter((comment) => comment.user.login === 'w3c-bots')
+        .filter((comment) => !noTestRe.test(comment.body))
         .map((comment) => [
             result.pullRequest.number,
             new Date(result.pullRequest.created_at).getTime(),
